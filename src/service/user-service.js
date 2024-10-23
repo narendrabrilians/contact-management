fae120b5ec6f44f757c1744b013b0b9d98f4d552
Narendra Febriliansyah R <narendrabrilians@gmail.com>
2024-10-23 13:11:18 +0700

    feat: add get user api

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,6 +1,7 @@
 import { prismaClient } from "../application/database.js";
 import { ResponseError } from "../error/response-error.js";
 import {
+  getUserValidation,
   loginUserValidation,
   registerUserValidation,
 } from "../validation/user-validation.js";
@@ -72,7 +73,28 @@ const login = async (request) => {
   });
 };
 
+const get = async (username) => {
+  const validUsername = validate(getUserValidation, username);
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      username: validUsername,
+    },
+    select: {
+      username: true,
+      name: true,
+    },
+  });
+
+  if (!user) {
+    throw new ResponseError(404, "User is not found");
+  }
+
+  return user;
+};
+
 export default {
   register,
   login,
+  get,
 };
